00255569d461bfb8d69aec5afe47aee33407d383
david.elahee@gmail.com <david.elahee@gmail.com>
2014-07-04 11:55:03 +0200

    cleaned up vtf sample

--- samples/vtf/Demo.hx
+++ samples/vtf/Demo.hx
@@ -1,19 +1,22 @@
 
 import flash.Lib;
-import format.abc.Context;
-import format.vtf.Data;
-import format.vtf.Reader;
+
+import hxd.fmt.vtf.Data;
+import hxd.fmt.vtf.Reader;
+
 import h2d.Anim;
 import h2d.Bitmap;
 import h2d.Text;
 import h3d.Engine;
+
 import haxe.io.BufferInput;
 import haxe.io.Bytes;
 import haxe.io.BytesInput;
+
 import haxe.macro.Format;
 import haxe.Resource;
 import haxe.Timer;
-import haxe.Utf8;
+
 import hxd.BitmapData;
 import h2d.SpriteBatch;
 import hxd.ByteConversions;
@@ -21,45 +24,32 @@ import hxd.Pixels;
 import hxd.Profiler;
 import hxd.res.Texture;
 import hxd.System;
-import mt.fx.Flash;
 import openfl.Assets;
-class Demo 
-{
+
+class Demo {
 	var engine : h3d.Engine;
 	var scene : h2d.Scene;
-	var actions : List < Void->Void > ;
 	
-	function new() 
-	{
+	function new() 	{
 		engine = new h3d.Engine();
 		engine.onReady = init;
 		engine.backgroundColor = 0xFFCCCCCC;
-		//engine.autoResize = true;
 		engine.init();
 		
-		#if flash
-		flash.Lib.current.addChild(new openfl.display.FPS());
-		#end
-		//flash.Lib.current.addEventListener(flash.events.Event.RESIZE, onResize );
 	}
 	
-	function onResize(_)
-	{
-		trace("resize");
-		trace(flash.Lib.current.stage.stageWidth + " " + flash.Lib.current.stage.stageHeight);
-	}
-	
-	function init() 
-	{
+	function init() {
+		
+		scene = new h2d.Scene();
 		
 		Profiler.begin("vtf");
-		var t = new format.vtf.Reader(ByteConversions.byteArrayToBytes(Assets.getBytes("assets/test_quad_2k.vtf")));
-		var d : format.vtf.Data = t.read();
+		var t = new hxd.fmt.vtf.Reader(ByteConversions.byteArrayToBytes(Assets.getBytes("assets/test_quad_2k.vtf")));
+		var d : hxd.fmt.vtf.Data = t.read();
 		var bmp = h2d.Bitmap.fromPixels( d.toPixels() , scene );
 		bmp.x += 32;
 		bmp.y += 32;
+		bmp.scaleX = bmp.scaleY = 0.1;
 		Profiler.end("vtf");
-		
 		
 		Profiler.begin("vtf zip");
 		var b = ByteConversions.byteArrayToBytes(Assets.getBytes("assets/test_quad_2k.vtf.zip"));
@@ -68,49 +58,32 @@ class Demo
 		entry.data = mt.Lib.inflate(entry.data, entry.fileSize);
 		entry.compressed = false;
 		
-		trace("reading! "+entry.data.length);
-		var t = new format.vtf.Reader(entry.data);
-		trace("making reader");
-		var d : format.vtf.Data = t.read();
-		trace("read!");
+		var t = new hxd.fmt.vtf.Reader(entry.data);
+		var d : hxd.fmt.vtf.Data = t.read();
 		var bmp = h2d.Bitmap.fromPixels( d.toPixels() , scene );
 		bmp.x += 32;
 		bmp.y += 32;
+		bmp.scaleX = bmp.scaleY = 0.1;
 		Profiler.end("vtf zip");
-		
-		
 		
 		Profiler.begin("png");
 		var bmp = h2d.Bitmap.create( hxd.BitmapData.fromNative( Assets.getBitmapData("assets/test_quad_2k.png")) , scene );
 		bmp.x += 256;
 		bmp.y += 32;
+		bmp.scaleX = bmp.scaleY = 0.1;
 		Profiler.end("png");
-		
-		trace("finished");
-		
-		scene = new h2d.Scene();
-		var root = new h2d.Sprite(scene);
-		
-		var fontRoboto = hxd.res.FontBuilder.getFont("Roboto-Black", 32, { antiAliasing : false , chars : hxd.Charset.DEFAULT_CHARS } );
 		
 		trace(Profiler.dump(false));
 		Profiler.clean();
 		
 		hxd.System.setLoop(update);
-		
-		
 	}
-	
-	static var fps : Text;
-	static var tf : Text;
 	
 	function update() 	{
 		engine.render(scene);
 	}
 	
-	static function main() 
-	{
-		//hxd.Res.loader = new hxd.res.Loader(hxd.res.EmbedFileSystem.create());
+	static function main() 	{
 		new Demo();
 	}
 }
