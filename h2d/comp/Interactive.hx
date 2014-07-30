00f0212efbc2fd5e91f0dcab810530ec9c6e36f5
Pascal Peridont <pperidont@motion-twin.com>
2014-07-30 15:04:54 +0200

    comp.Box extends comp.Interactive (allow <div onclick="">)

--- h2d/comp/Interactive.hx
+++ h2d/comp/Interactive.hx
@@ -2,12 +2,22 @@ package h2d.comp;
 
 class Interactive extends Component {
 	
+	public var hasInteraction : Bool;
 	var input : h2d.Interactive;
+	var needInput : Bool;
 	var active : Bool;
 	var activeRight : Bool;
 	
 	function new(kind,?parent) {
 		super(kind,parent);
+
+		needInput = kind!="box";
+		hasInteraction = kind!="box";
+		if( needInput )
+			initInput();
+	}
+
+	function initInput(){
 		input = new h2d.Interactive(0, 0, bg);
 		input.enableRightButton = true;
 		active = false;
@@ -51,9 +61,17 @@ class Interactive extends Component {
 	override function resize( ctx : Context ) {
 		super.resize(ctx);
 		if( !ctx.measure ) {
-			input.width = width - (style.marginLeft + style.marginRight);
-			input.height = height - (style.marginTop + style.marginBottom);
-			input.visible = !hasClass(":disabled");
+			if( needInput ){
+				if( input == null )
+					initInput();
+				input.cursor = hasInteraction ? Button : Default;
+				input.width = width - (style.marginLeft + style.marginRight);
+				input.height = height - (style.marginTop + style.marginBottom);
+				input.visible = !hasClass(":disabled");
+			}else if( input != null ){
+				input.remove();
+				input = null;
+			}
 		}
 	}
 	
