c787f98fdf83369a84487c3b042bc698c20c0031
david.elahee@gmail.com <david.elahee@gmail.com>
2014-09-11 11:40:48 +0200

    first pass of adding auto-atlasing capabilities

--- tools/convert/Convert.hx
+++ tools/convert/Convert.hx
@@ -25,6 +25,8 @@ import hxd.Profiler;
 import hxd.res.LocalFileSystem;
 import hxd.System;
 
+import format.png.Data;
+
 using StringTools;
 
 class Convert {
@@ -37,9 +39,11 @@ class Convert {
 	var animMode : h3d.fbx.Library.AnimationMode = h3d.fbx.Library.AnimationMode.LinearAnim;
 	var anim : Animation = null;
 	var saveAnimsOnly = false;
-	var saveModelOnly = false; // no anmations
+	var saveModelOnly = false; // no animations
 	var verbose = #if debug true #else false #end;
 	
+	//todo change
+	var makeAtlas = true;
 	function new() {
 		hxd.System.debugLevel = 0;
 		start();
@@ -71,6 +75,10 @@ class Convert {
 					
 				case "-v","--verbose":
 					verbose = true;
+					
+				case "--make-atlas":
+					makeAtlas = true;
+					
 					
 				default: pathes.push( arg );
 			}
@@ -104,7 +112,7 @@ class Convert {
 			fref.addEventListener(flash.events.Event.COMPLETE, function(_){
 			var file = (haxe.io.Bytes.ofData(fref.data)).toString();
 			#end
-				loadData(file);
+				loadData(path,file);
 				
 				scene.traverse(function(obj) {
 					trace("read " + obj.name);
@@ -112,6 +120,44 @@ class Convert {
 						trace("parent is :" + obj.parent.name);
 				});
 					
+				
+				//add filters or process here
+				
+				if (makeAtlas) {
+					var allMat : Map<String,h3d.mat.Texture> = new Map();
+					
+					scene.traverse(function(obj:h3d.scene.Object) {
+						if ( obj.isMesh()) {
+							var m  = obj.toMesh();
+							var name = m.material.texture.name;
+							var tex = m.material.texture;
+							allMat.set( name, tex );
+							
+							trace( "detected texture:"+name +" tex.width:"+tex.width);
+						}
+					});
+						
+					/*
+					scene.traverse(function(obj) {
+						if ( obj.isMesh()) {
+							var m  = obj.toMesh();
+							trace( m.material.name );
+						}
+					});
+					*/
+					
+					if (false ) {
+							/*
+						var packer = new hxd.tools.Packer();
+						var res : flash.display.BitmapData = packer.process();
+						var bytes = hxd.ByteConversions.byteArrayToBytes(res.getPixels(res.rect));
+						var out = new sys.io.BytesOutput(bytes);
+						var w = new format.png.Writer( out  );
+						w.write( format.png.Tools.build32ARGB(bytes));
+						*/
+					}
+				}
+				
 				if( saveAnimsOnly )
 					saveAnimation(path);
 				else if( saveModelOnly )
@@ -132,16 +178,47 @@ class Convert {
 		return;
 	}
 	
-	function loadData( data : String, newFbx = true ) {
+	var bitmaps:Map<String,flash.display.BitmapData> = new Map();
+	
+	function loadData( path:String,data : String, newFbx = true ) {
 		curFbx = new h3d.fbx.Library();
 		var fbx = h3d.fbx.Parser.parse(data);
 		curFbx.load(fbx);
 		var frame = 0;
 		var o : h3d.scene.Object = null;
 		scene.addChild(o = curFbx.makeObject( function(str, mat) {
-			var m = h3d.mat.Texture.fromColor(0xFFFF00FF);
-			m.name = str;
-			return new MeshMaterial(m);
+			var root = sys.FileSystem.fullPath(path);
+			root = root.replace("\\", "/");
+			var dir = root.split("/");
+			dir.splice(dir.length - 1, 1);
+			root = dir.join("/");
+			str = root +"/" + str;
+			if ( !sys.FileSystem.exists(str) ) {
+				var m = h3d.mat.Texture.fromColor(0xFFFF00FF);
+				m.name = str;
+				return new MeshMaterial(m);
+			}
+			else {
+				var bytes = sys.io.File.getBytes( str );
+				var bi = new haxe.io.BytesInput(bytes);
+				var data = new format.png.Reader( bi ).read();
+				var header : format.png.Data.Header=null;
+				for ( l in data) {
+					switch(l) {
+						case CHeader(h): header = h;
+					default:
+					}
+				}
+				var bmdBytes = format.png.Tools.extract32(data);
+				var bmd = new flash.display.BitmapData(0,0,true);
+				bmd.setPixels( new flash.geom.Rectangle(0, 0, header.width, header.height), hxd.ByteConversions.bytesToByteArray( bmdBytes ));
+				var m = h3d.mat.Texture.fromBitmap(hxd.BitmapData.fromNative( bmd ));
+				m.name = str;
+				
+				bitmaps.set( str, bmd );
+				
+				return new MeshMaterial(m);
+			}
 		}));
 		setSkin(o);
 		trace("loaded " + o.name);
