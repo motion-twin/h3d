d78ac499df068c4f4bdf931c6cef1a78dab374e6
david.elahee@gmail.com <david.elahee@gmail.com>
2015-01-14 12:28:08 +0100

    minor format

--- hxd/Behaviour.hx
+++ hxd/Behaviour.hx
@@ -1,10 +1,12 @@
 package hxd;
 
 class Behaviour {
-	var 	obj:h3d.scene.Object;
-	public function new(o: h3d.scene.Object) { obj = o;  o.addBehaviour(this); }
-	public function dispose() { obj.removeBehaviour(this); obj = null; }
-	public function update() { }
+	public var 		obj(default, null):h3d.scene.Object;
+	
+	public function new(o: h3d.scene.Object) 				{ obj = o;  o.addBehaviour(this); }
+	
+	public function dispose() 								{ obj.removeBehaviour(this); obj = null; }
+	public function update() 								{ }
 
 	public function clone(c) : hxd.Behaviour {
 		throw "Please implement me";
