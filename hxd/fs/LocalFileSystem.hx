a399cbac10f96017f8b8394989fa8af0e6a9f36f
Pascal Peridont <pperidont@motion-twin.com>
2018-02-28 09:32:27 +0100

    Added fonts & atlas conversion in LocalFileSystem

--- hxd/fs/LocalFileSystem.hx
+++ hxd/fs/LocalFileSystem.hx
@@ -301,6 +301,7 @@ class LocalFileSystem implements FileSystem {
 		addConvert(new Convert.ConvertFBX2HMD());
 		addConvert(new Convert.ConvertTGA2PNG());
 		addConvert(new Convert.ConvertFNT2BFNT());
+		addConvert(new ConvertAtlas());
 		#if flash
 		var froot = new flash.filesystem.File(flash.filesystem.File.applicationDirectory.nativePath + "/" + baseDir);
 		if( !froot.exists ) throw "Could not find dir " + dir;
